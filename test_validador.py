5952eb3c185a61e807dac6ff375b1ef605a305a0
ErickPineda67 <2025310040@uteq.edu.mx>
2026-07-23 23:38:03 -0600

    Update test_validador.py

--- test_validador.py
+++ test_validador.py
@@ -1,4 +1,4 @@
-#Paso 1 (Erick Froylan Pineda Aguillon - Escribe la prueba)
+#Paso 1 (Erick Froylan Pineda Aguillon)
 import pytest
 from validador import es_password_valida
 
@@ -6,6 +6,3 @@ def test_password_corta_devuelve_false():
     password_corta = "abc"
     resultado = es_password_valida(password_corta)   
     assert resultado is False
-
-def es_password_valida(password: str) -> bool:
-    return len(password) >= 8
