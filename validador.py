0d10ecc917392d75eaf288bb4a695b859da8a421
ErickPineda67 <2025310040@uteq.edu.mx>
2026-07-23 23:52:33 -0600

    Update validador.py

--- validador.py
+++ validador.py
@@ -1,2 +1,4 @@
 def es_password_valida(password: str) -> bool:
-    return len(password) >= 8
+    tiene_longitud = len(password) >= 8
+    tiene_numero = any(caracter.isdigit() for caracter in password)
+    return tiene_longitud and tiene_numero
